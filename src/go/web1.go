471b009222fbbd5fd26d391f932eafb7900053c2
hall liu <lyp1984831@hotmail.com>
2019-04-24 22:26:16 +0800

    push changes for go example

---
+++ src/go/web1.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	// "fmt"
+	"io/ioutil"
+	"net/http"
+	"log"
+	"html/template"
+)
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+func main() {
+	// p1 := &Page{Title: "test", Body: []byte("test again")}
+	// p1.save()
+	// p2, _ := load("test")
+	// fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+

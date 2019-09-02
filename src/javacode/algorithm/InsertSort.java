2fb127694965ccb43a1dc9130fcd9c80c00e7f82
lyp1984831@hotmail.com <lyp1984831@hotmail.com>
2019-09-02 22:14:42 +0800

    update code

--- src/javacode/algorithm/InsertSort.java
+++ src/javacode/algorithm/InsertSort.java
@@ -6,25 +6,21 @@ public class InsertSort {
     public static void main(String[] args) {
         int[] randomNumberArray = {6, 5, 7, 9, 2, 3, 8, 4};
         int arrayLength = randomNumberArray.length;
-        int[] result = new int[arrayLength];
+
         System.out.println("The number array content before sort");
         System.out.println(Arrays.toString(randomNumberArray));
 
-        result[0] = randomNumberArray[0];
+
         for (int startIndex = 1; startIndex < arrayLength; startIndex++) {
-            for (int insertIndex = startIndex - 1; insertIndex >= 0; insertIndex--) {
-                int temp = 0;
-                if (result[insertIndex] > randomNumberArray[startIndex]) {
-                    temp = result[insertIndex];
-                    result[insertIndex] = randomNumberArray[startIndex];
-                    result[startIndex] = temp;
-                } else {
-		    result[startIndex] = randomNumberArray[startIndex];
-		    break;
+            for (int insertIndex = 0; insertIndex <= startIndex; insertIndex++) {
+                if (randomNumberArray[insertIndex] > randomNumberArray[startIndex]) {
+                    int temp = randomNumberArray[insertIndex];
+                    randomNumberArray[insertIndex] = randomNumberArray[startIndex];
+                    randomNumberArray[startIndex] = temp;
                 }
             }
         }
         System.out.println("The number array content after sort from low to high");
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(randomNumberArray));
     }
 }
